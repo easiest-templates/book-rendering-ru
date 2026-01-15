a444d3c57769cdd2ca16539833d2e2780354393d
qanelph <dd@ddostup.ru>
2026-01-16 02:46:04 +0300

    Initial commit: КнижныйМир - книжный магазин с 3D визуализацией
    - React 18 + TypeScript
    - Three.js + React Three Fiber для 3D рендеринга
    - Tailwind CSS + Radix UI
    - Интерактивная 3D модель книги

---
+++ src/pages/Index.tsx
@@ -0,0 +1,11 @@
+import BookShowcase from "@/components/book-showcase"
+
+const Index = () => {
+  return (
+    <main className="min-h-screen">
+      <BookShowcase />
+    </main>
+  )
+}
+
+export default Index
